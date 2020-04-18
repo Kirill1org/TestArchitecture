6e1d1684b8cac1838337b7eb4602dc31c4c1bc0e
Kirill1org <39536848+Kirill1org@users.noreply.github.com>
2020-04-18 20:33:41 +0300

    Initial commit

---
+++ app/src/main/java/bonch/dev/testarchitecture/TestFragment.java
@@ -0,0 +1,29 @@
+package bonch.dev.testarchitecture;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class TestFragment extends Fragment {
+
+    private Button button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button = view.findViewById(R.id.button);
+    }
+}
